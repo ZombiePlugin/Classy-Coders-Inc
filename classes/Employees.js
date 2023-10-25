9d78ed3b86632ab9275978d83e42dfd18ac3e4b6
M <muzameelahmed1@gmail.com>
2023-10-25 15:23:07 +0100

    classes

--- classes/Employees.js
+++ classes/Employees.js
@@ -1,10 +1,41 @@
 class Employees {
-
-
-
-    
+  #salary;
+  #isHired = true;
+  #allEmployees = [];
+  constructor(name, position, salary) {
+    this.name = name;
+    this.position = position;
+    this.#salary = salary;
+    this.#allEmployees.push(this);
+  }
+  getSalary() {
+    return this.#salary;
+  }
+  setSalary(amount) {
+    this.#salary = amount;
+  }
+  getStatus() {
+    return this.#isHired;
+  }
+  setStatus(command) {
+    if (command === "fire") {
+      this.#isHired = false;
+    } else {
+      this.#isHired = true;
+    }
+  }
+  static getEmployees() {
+    return this.allEmployees;
+  }
+  static getTotalPayroll() {
+    let sum = 0;
+    for (let i = 0; i < this.allEmployees.length; i++) {
+      sum += this.allEmployees[i].salary;
+    }
+    return sum;
+  }
 }
 
 module.exports = {
-    Employees,
-}
+  Employees,
+};
--- classes/SalesPerson.js
+++ classes/SalesPerson.js
@@ -1,10 +1,21 @@
-class SalesPerson {
+const { Employees } = require("./Employees");
 
+class SalesPerson extends Employees {
+  #totalSales;
+  constructor(name, position, salary, clients) {
+    super(name, position, salary);
+    this.clients = clients;
+    this.#totalSales = 0;
+  }
 
-
-    
+  getSalesNumbers() {
+    return this.#totalSales;
+  }
+  makeSale(amount) {
+    this.#totalSales = amount;
+  }
 }
 
 module.exports = {
-    SalesPerson,
-}
+  SalesPerson,
+};
